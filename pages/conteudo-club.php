20d3f31adcb21aee8ff15ac410f6a37e38798c07
claytonKuiaski <claytonKuiaski@hotmail.com>
2016-12-20 00:29:48 -0200

    finalização

--- pages/conteudo-club.php
+++ pages/conteudo-club.php
@@ -1,3 +1,35 @@
-<div class="container">
-	<h2>miolo THE CLUB</h2>
-</div>
+	<div class="container">
+		<div class="conteudo">
+			<h1 class="wow flip" data-wow-duration="1s" data-wow-delay="0s">membership benefits</h1><hr>
+			<div class="membership">
+				<div class="souvenirs">
+					<img src="images/souvenirs.png">
+					<h3>monthly<br>souvenirs</h3>
+				</div>
+				<div class="events">
+					<img src="images/events.png">
+					<h3>events</h3>
+				</div>
+				<div class="vip">		
+					<img src="images/vip.png">
+					<h3>vip entrance</h3>
+				</div>
+				<div class="business">		
+					<img src="images/business.png">
+					<h3>business meetings</h3>
+				</div>
+				<div class="card">		
+					<img src="images/card.png">
+					<h3>credit card</h3>
+				</div>
+			</div>
+			<hr>
+			<div class="member">
+				<ul>	
+					<li>
+						<a href="">become a member</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>	
